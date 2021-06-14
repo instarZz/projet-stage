546c9c5232ed0c8e258e8f287147951b5d9711f5
instarZz <instarz.je@gmail.com>
2021-06-14 17:26:25 +0200

    WIP find by id

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+$(document).ready(async function(){
 
     const $form = $('#form');
     const $containerForm = $('#containerForm');
@@ -8,14 +8,29 @@ $(document).ready(function(){
 
     $containerForm.css({"display":"none"});
 
-    $iconsEdit.on('click', function(){
+    $iconsEdit.on('click', function(event){
+        const teamId = $(event.currentTarget).attr("data-team-id");
+        console.log(teamId);
+
+        const response = await fetch('http://localhost:3000/getTeams', {
+            method: 'GET',
+        });
+        const data = await response.json();
+        console.log(data);
+
         $containerForm.css({"display":"flex"});
         $body.css({"overflow":"hidden"});
+
     });
 
     $iconClose.on('click', function(){
         $containerForm.css({"display":"none"});
         $body.css({"overflow":"auto"});
     });
+
+
+
+
+        
 
 });
--- routes/index.js
+++ routes/index.js
@@ -1,28 +1,49 @@
 var express = require('express');
 var app = express()
 var router = express.Router();
+const db = [
+  {
+    id:1,
+    name:"Equipe44",
+    email:"equipe@team.gg",
+    phoneNumber:"06 66 66 66 66",
+    address:"4 rue du quatre 44444 Quatre"
+  }, 
+  {
+    id:2,
+    name:"Equipe2",
+    email:"equipe2@team.gg",
+    phoneNumber:"06 61 66 66 66",
+    address:"4 rue du quatre 44444 Quatre"
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Pannel Equipes' });
+  const teamsData = getTeams();
+  res.render('index', 
+  { 
+    title: 'Pannel Equipes',
+    teamData: teamsData, 
+  });
 });
 
 router.get('/getTeams', function(req, res, next) {
-  res.send([
-    {
-      name:"Equipe",
-      email:"equipe@team.gg",
-      phoneNumber:"06 66 66 66 66",
-      address:"4 rue du quatre 44444 Quatre"
-    }, 
-    {
-      name:"Equipe2",
-      email:"equipe2@team.gg",
-      phoneNumber:"06 61 66 66 66",
-      address:"4 rue du quatre 44444 Quatre"
-    }
-  ]);
+  res.send(getTeams());
 });
+
+function getTeams() {
+  return db;
+}
+
+router.get('/getTeam', function(req, res, next) {
+  const teamId = req.query.teamId;
+  res.send(getTeam(teamId));
+});
+
+function getTeam() {
+  
+}
 
 app.use(express.static('public'));
 module.exports = router;
