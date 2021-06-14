54abc8589b8f28605f1505baa28ca4e306c9f7ba
instarZz <instarz.je@gmail.com>
2021-06-14 14:00:57 +0200

    Teams added

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Pannel Equipes' });
+});
+
+router.get('/getTeams', function(req, res, next) {
+  res.send([
+    {
+      name:"Equipe",
+      email:"equipe@team.gg",
+      phoneNumber:"06 66 66 66 66",
+      address:"4 rue du quatre 44444 Quatre"
+    }, 
+    {
+      name:"Equipe2",
+      email:"equipe2@team.gg",
+      phoneNumber:"06 61 66 66 66",
+      address:"4 rue du quatre 44444 Quatre"
+    }
+  ]);
 });
 
 app.use(express.static('public'));
 module.exports = router;
+
