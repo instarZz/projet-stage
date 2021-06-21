a4406580c8c480fef0718ada6cf107421e921d03
instarZz <instarz.je@gmail.com>
2021-06-21 16:20:12 +0200

    popup form OK

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,32 +1,73 @@
 $(document).ready(async function(){
 
-    const $form = $('#form');
-    const $containerForm = $('#containerForm');
-    const $iconsEdit = $('.js-edit');
-    const $iconClose = $('.icon-close');
-    const $body = $('body');
-    const $submitBtn = $('#submit-btn');
-    const $formInput = $('.form-input');
+    const $form = $('#form'),
+    $containerForm = $('#containerForm'),
+    $iconsEdit = $('.js-edit'),
+    $iconClose = $('.icon-close'),
+    $body = $('body'),
+    $submitBtn = $('#submit-btn'),
+    $formInput = $('.form-input'),
+    $labelElt = $('.label-elt'),
+    $containerInput = $('.container-input');
+    
+    // $formInput.on('focus', (event) => {
+    //     const $input = $(event.currentTarget);
+        
+    //     $input.next().removeClass('big-label').addClass('small-label');
+    // });
+    // $formInput.on('keyup', function(event){
+    //     const target = event.currentTarget;
+    //     handleLabelChange(target);
+    // }); 
+
+
 
     $containerForm.css({"display":"none"});
+    
+    $iconsEdit.on('click', async function(event){       
+            
+            $containerForm.css({"display":"flex"});
+            $body.css({"overflow":"hidden"});
+            
+            $submitBtn.html('Modifier');
 
-    $iconsEdit.on('click', async function(event){
+            const teamId = $(event.currentTarget).attr("data-team-id");
 
+            await populateForm(teamId, $formInput);
+            // initLabel($formInput);
 
-        // const response = await fetch('http://localhost:3000/getTeams', {
-        //     method: 'GET',
-        // });
-        // console.log(data);
-        
-        // const id = data_team_id
-        
-        $containerForm.css({"display":"flex"});
-        $body.css({"overflow":"hidden"});
-        
-        $submitBtn.html('Modifier');
-        
-        const teamId = $(event.currentTarget).attr("data-team-id");
-        // console.log(teamId);
+        });
+
+    // function getInputVal($input) {
+    //     val = $input.val();
+    //     return val;
+    // }
+
+    // function editLabel(val, $label, $input) {
+    //     if(val==='') {
+    //         $label.addClass('big-label');
+    //         $label.removeClass('small-label');            
+    //     } else {
+    //         $label.removeClass('big-label');
+    //         $label.addClass('small-label');
+    //     }
+    // }
+
+    // function handleLabelChange(input){
+    //     const $input = $(input);
+    //     const val = getInputVal($input);
+    //     const $label = $input.next();
+    //     editLabel(val, $label, $input);
+
+    // }
+
+    // function initLabel($formInput) {
+    //     $formInput.each((i, input)=>{
+    //         handleLabelChange(input);
+    //     });  
+    // };
+
+    async function populateForm(teamId) {
         const response = await fetch(`http://localhost:3000/getTeam?id=${teamId}`, {
             method: 'GET',
         });
@@ -37,18 +78,18 @@ $(document).ready(async function(){
             const val = data[dataType];
             $elt.val(val);
         });
-    });
+    };
 
     $iconClose.on('click', function(){
         $containerForm.css({"display":"none"});
         $body.css({"overflow":"auto"});
     });
-
     
-
-
-
-
-        
-
+    
+    
+    
+    
+    
+    
+    
 });
